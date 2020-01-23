c19b303e97314a21af43719e21fa235d8a59e61e
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-23 15:34:18 +0100

    Add scaffolding for components related to streams

---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamCreate = () => {
+  return <div>Hi from StreamCreate</div>;
+};
+
+export default StreamCreate;
---
+++ src/components/streams/StreamEdit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamEdit = () => {
+  return <div>Hi from StreamEdit</div>;
+};
+
+export default StreamEdit;
