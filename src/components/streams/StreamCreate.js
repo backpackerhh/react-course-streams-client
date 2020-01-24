acb2aae05fde1f41c17d467d7e9eec7304c8b26a
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-24 14:25:24 +0100

    Allow to validate and display errors if necessary when form is submitted

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,13 +2,27 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 const StreamCreate = props => {
-  const renderInput = ({ input, label }) => {
+  const renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.touched && meta.error ? "error" : ""}`;
+
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+
+        {renderErrorIfNecessary(meta)}
       </div>
     );
+  };
+
+  const renderErrorIfNecessary = ({ touched, error }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
   };
 
   const onSubmit = formValues => {
@@ -16,7 +30,7 @@ const StreamCreate = props => {
   };
 
   return (
-    <form className="ui form" onSubmit={props.handleSubmit(onSubmit)}>
+    <form className="ui form error" onSubmit={props.handleSubmit(onSubmit)}>
       <Field name="title" component={renderInput} label="Title" />
       <Field name="description" component={renderInput} label="Description" />
 
@@ -25,6 +39,21 @@ const StreamCreate = props => {
   );
 };
 
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
 export default reduxForm({
-  form: "streamCreate"
+  form: "streamCreate",
+  validate
 })(StreamCreate);
