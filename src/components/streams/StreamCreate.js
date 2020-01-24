49f1acc186f4d904f3eb8fdf7a4aeda490830a57
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-24 13:39:53 +0100

    Allow to submit form to create streams using Redux Form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>Hi from StreamCreate</div>;
+const StreamCreate = props => {
+  const renderInput = ({ input, label }) => {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  };
+
+  const onSubmit = formValues => {
+    console.log(formValues);
+  };
+
+  return (
+    <form className="ui form" onSubmit={props.handleSubmit(onSubmit)}>
+      <Field name="title" component={renderInput} label="Title" />
+      <Field name="description" component={renderInput} label="Description" />
+
+      <button className="ui button primary">Submit</button>
+    </form>
+  );
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
