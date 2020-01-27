5bff32c526c5a1e8e69dd1f143864bc93d8e43a9
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-27 13:06:41 +0100

    Allow to display edit and delete button only to stream owner

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -12,6 +12,8 @@ const StreamList = props => {
     return props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {renderAdmin(stream)}
+
           <i className="large middle aligned icon camera"></i>
 
           <div className="content">
@@ -24,6 +26,17 @@ const StreamList = props => {
     });
   };
 
+  const renderAdmin = stream => {
+    if (stream.userId === props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  };
+
   return (
     <div>
       <h2>Streams</h2>
@@ -33,7 +46,10 @@ const StreamList = props => {
 };
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
