5bdf36699258ddfbe2cb091e3c96d8ab256f0d54
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-27 12:26:08 +0100

    Allow to fetch and display streams list

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 
-const StreamList = () => {
-  return <div>Hi from StreamList</div>;
+import { fetchStreams } from "../../actions";
+
+const StreamList = props => {
+  useEffect(() => {
+    props.fetchStreams();
+  }, []);
+
+  const renderList = () => {
+    return props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera"></i>
+
+          <div className="content">
+            {stream.title}
+
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h2>Streams</h2>
+      <div className="ui celled list">{renderList()}</div>
+    </div>
+  );
 };
 
-export default StreamList;
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
