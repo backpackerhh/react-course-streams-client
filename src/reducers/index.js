9a5095ef6322c1dcdbe988dd7e6e947b4f5962da
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-27 11:17:09 +0100

    Add all reducers for streams

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 
 import authReducer from "../reducers/authReducer";
+import streamsReducer from "../reducers/streamsReducer";
 
 export default combineReducers({
   auth: authReducer,
-  form: formReducer
+  form: formReducer,
+  streams: streamsReducer
 });
