9bb8e3c0ef813f9e9ab811e59a232bfb38e0a162
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-28 13:39:18 +0100

    Refactor function component to class component to be able to use refs

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -3,22 +3,34 @@ import { connect } from "react-redux";
 
 import { fetchStream } from "../../actions";
 
-const StreamShow = props => {
-  useEffect(() => {
-    props.fetchStream(props.match.params.id);
-  }, []);
+class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
 
-  if (!props.stream) {
-    return <div>Loading...</div>;
+    this.videoRef = React.createRef();
   }
 
-  return (
-    <div>
-      <h1>{props.stream.title}</h1>
-      <h5>{props.stream.description}</h5>
-    </div>
-  );
-};
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    const { title, description } = this.props.stream;
+
+    return (
+      <div>
+        <video ref={this.videoRef}></video>
+
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
