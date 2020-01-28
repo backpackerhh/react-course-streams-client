9a27131da304ee1ed439b5ef53d5f41330fbf288
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-28 10:13:48 +0100

    Use PATCH request to update a stream
    The PATCH request allows us to update just a few properties of the
    object, unlike the PUT request where all properties must be provided
    with the request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -63,7 +63,7 @@ export const deleteStream = id => async dispatch => {
 };
 
 export const updateStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({
     type: UPDATE_STREAM,
